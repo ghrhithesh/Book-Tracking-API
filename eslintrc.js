430911513f0f322c4463c3d867cd1c7172297b37
ghrhithesh <ghrhithesh1511@gmail.com>
2025-05-27 17:30:14 +0530

    add ESLint and Prettier configurations for code quality and formatting

---
+++ eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    '@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-console': 'off',
+  },
+};
